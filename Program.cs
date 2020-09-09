e0e5a676bd419e52738d7ac8af0d4949b92f786f
Kami <kidfu@live.com>
2020-09-09 16:37:24 +1200

    Finish Q1389

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            
+            Q1389.Run(new[] {0, 1, 2, 3, 4}, new[] {0, 1, 2, 2, 1});
+            Q1389.Run(new[] {1, 2, 3, 4, 0}, new[] {0, 1, 2, 3, 0});
+            Q1389.Run(new[] {1}, new[] {0});
         }
     }
 }
--- Questions/Q1389.cs
+++ Questions/Q1389.cs
@@ -6,44 +6,76 @@ namespace LeetCodeSolution.Questions
 {
     public class Q1389
     {
-        public static void Run(int[] nums, int[] index){
+        public static void Run(int[] nums, int[] index)
+        {
             Solution s = new Solution();
-            Console.WriteLine(s.CreateTargetArray(nums, index));
+            Array.ForEach(s.CreateTargetArray(nums, index), Console.WriteLine);
         }
 
-        public class Solution {
-            public int[] CreateTargetArray(int[] nums, int[] index) {
-                int[] target = new int[nums.Length];
-                for(int i = 0; i < target.Length;i++){
-                    target[i] = -1;
+        public class Solution
+        {
+            public int[] CreateTargetArray(int[] nums, int[] index)
+            {
+                // int[] target = new int[nums.Length];
+                // for (int i = 0; i < target.Length; i++)
+                // {
+                //     target[i] = -1;
+                // }
+                //
+                // for (int i = 0; i < index.Length; i++)
+                // {
+                //     ArrayInsertAt(target, index[i], nums[i]);
+                // }
+                //
+                // return target;
+                List<int> list = new List<int>();
+                for (int i = 0; i < nums .Length ; i++)
+                {
+                    if (index [i]>list .Count )
+                    {
+                        list.Add(nums[i]);
+                    }
+                    else if (index [i]<=list .Count )
+                    {
+                        list.Insert(index[i], nums[i]);
+                    }
                 }
-
-                for(int i = 0; i < index.Length; i++){
-                    ArrayInsertAt(target, index[i], nums[i]);
-                }
-                
-                return target;
+                int[] result = list.ToArray();
+                return result;
             }
 
-            public void ArrayInsertAt(int[] nums, int index, int num){
-                if(nums[index] == -1){
+            public void ArrayInsertAt(int[] nums, int index, int num)
+            {
+                if (nums[index] == -1)
+                {
                     nums[index] = num;
                     return;
                 }
-                if(index < nums.Length && index > -1){
-                    int temp = 0;
-                    for(int i = index + 1, i < nums.Length; i++){
-                        if(nums[i] == -1){
-                            nums[i] = temp;
+
+                if (index < nums.Length && index > -1)
+                {
+                    int pre = nums[index];
+                    int cur = nums[index];
+                    for (int i = index; i < nums.Length; i++)
+                    {
+                        if (nums[i + 1] == -1)
+                        {
+                            nums[i + 1] = pre;
                             break;
-                        }else{
-                            temp = nums[i+1];
-                            nums[i+1] = nums[i];
+                        }
+                        else
+                        {
+                            cur = nums[i + 1];
+                            nums[i + 1] = pre;
+                            pre = cur;
                         }
                     }
-                }else{
+                }
+                else
+                {
                     return;
                 }
+
                 nums[index] = num;
             }
         }
