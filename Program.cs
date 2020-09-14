91ad894e04bfecd49c66a8e739deacf059c330d6
Kami <kidfu@live.com>
2020-09-14 21:46:37 +1200

    Add Q1365

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            Q807.Run(new[] {new[] {3, 0, 8, 4}, new[] {2, 4, 5, 7}, new[] {9, 2, 6, 3}, new[] {0, 3, 1, 0}});
+            Q1365.Run(new []{6,5,4,8});
+            Q1365.Run(new []{7,7,7,7});
         }
     }
 }
---
+++ Questions/Q1365.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCodeSolution.Questions
+{
+    public class Q1365
+    {
+        public static void Run(int[] nums)
+        {
+            Solution s =new Solution();
+            Array.ForEach(s.SmallerNumbersThanCurrent(nums),Console.WriteLine);
+        }
+        public class Solution {
+            public int[] SmallerNumbersThanCurrent(int[] nums)
+            {
+                int[] rtn = new int[nums.Length];
+                
+                Array.Copy(nums,rtn,nums.Length);
+                
+                Array.Sort(nums);
+
+                for (int i = 0; i < rtn.Length; i++)
+                {
+                    var num = rtn[i];
+                    rtn[i] = Array.IndexOf(nums, num);
+                }
+
+                return rtn;
+            }
+        }
+    }
+}
