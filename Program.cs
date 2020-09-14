5a338092c039ce0b1cbefd17156aa72016be5617
Kami <kidfu@live.com>
2020-09-14 21:35:54 +1200

    Add Q807

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            Q1342.Run(14);
-            Q1342.Run(8);
-            Q1342.Run(123);
+            Q807.Run(new[] {new[] {3, 0, 8, 4}, new[] {2, 4, 5, 7}, new[] {9, 2, 6, 3}, new[] {0, 3, 1, 0}});
         }
     }
 }
---
+++ Questions/Q807.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+
+namespace LeetCodeSolution.Questions
+{
+    public class Q807
+    {
+        public static void Run(int[][] grid)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.MaxIncreaseKeepingSkyline(grid));
+        }
+
+        public class Solution
+        {
+            public int MaxIncreaseKeepingSkyline(int[][] grid)
+            {
+                int rtn = 0;
+                int xLength = grid.Length;
+                int yLength = grid[0].Length;
+                int[] xArr = new int[grid.Length];
+                int[] yArr = new int[grid[0].Length];
+                int x = 0, y = 0;
+                // build arr
+                for (int i = 0; i < xLength * yLength; i++)
+                {
+                    x = i / yLength;
+                    y = i % yLength;
+                    // update x
+                    if (grid[x][y] > xArr[x])
+                    {
+                        xArr[x] = grid[x][y];
+                    }
+                    // update y
+                    if (grid[x][y]>yArr[y])
+                    {
+                        yArr[y] = grid[x][y];
+                    }
+                }
+                
+                // calculate build
+                for (int i = 0; i < xLength * yLength; i++)
+                {
+                    x = i / yLength;
+                    y = i % yLength;
+                    if (xArr[x] < yArr[y])
+                    {
+                        rtn += (xArr[x] - grid[x][y]);
+                    }
+                    else
+                    {
+                        rtn += (yArr[y] - grid[x][y]);
+                    }
+                }
+
+                return rtn;
+            }
+        }
+    }
+}
