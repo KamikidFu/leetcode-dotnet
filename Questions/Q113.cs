599596b0e14b0a65add3a30fc33d2bac25882c3f
Kami <kidfu@live.com>
2020-09-27 22:59:56 +1300

    Finish Q113

--- Questions/Q113.cs
+++ Questions/Q113.cs
@@ -1,62 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using LeetCodeSolution.Structure;
 
 namespace LeetCodeSolution.Questions
 {
     public class Q113
     {
-        private IList<IList<int>> rtnList;
         public static void Run()
         {
-            rtnList = new List<List<int>>();
             Solution s = new Solution();
-            int sum = 22;
-            TreeNode root = new TreeNode(5);
-            root.left = new TreeNode(4);
-            root.left.left = new TreeNode(11);
-            root.left.left.left = new TreeNode(7);
-            root.left.left.right = new TreeNode(2);
-            root.right = new TreeNode(8);
-            root.right.left = new TreeNode(13);
-            root.right.right = new TreeNode(4);
-            root.right.right.left = new TreeNode(5);
-            root.right.right.right = new TreeNode(1);
+            int sum = 2;
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(-2);
+            root.left.left = new TreeNode(1);
+            root.left.left.left = new TreeNode(-1);
+            root.left.right = new TreeNode(3);
+            root.right = new TreeNode(-3);
+            root.right.left = new TreeNode(-2);
             var res = s.PathSum(root, sum);
         }
-        
 
-        public class Solution {
-            public IList<IList<int>> PathSum(TreeNode root, int sum) {
+
+        public class Solution
+        {
+            private int sum;
+            private IList<IList<int>> rtnList;
+
+            public IList<IList<int>> PathSum(TreeNode root, int sum)
+            {
                 IList<int> path = new List<int>();
-                if(root == null){
-                    return null;
+                this.rtnList = new List<IList<int>>();
+                this.sum = sum;
+                if (root == null)
+                {
+                    return this.rtnList;
                 }
-                if(root.left == null && root.right == null){
-                    path.add(root.val);
-                    rtnList.add(path);
+
+                path.Add(root.val);
+                if (root.left == null && root.right == null)
+                {
+                    if (root.val == sum)
+                    {
+                        rtnList.Add(new List<int>(path));
+                    }
                     return rtnList;
                 }
-                if(root.left != null){
+
+                if (root.left != null)
+                {
                     traverse(root.left, path);
                 }
-                if(root.right != null){
+
+                if (root.right != null)
+                {
                     traverse(root.right, path);
                 }
+
                 return rtnList;
             }
-            private void traverse(TreeNode tree, IList<int> path){
-                    path.add(tree.val);
-                if(tree.left == null && tree.right == null){
-                    rtnList.add(path);
+
+            private void traverse(TreeNode tree, IList<int> path)
+            {
+                path.Add(tree.val);
+                if (tree.left == null && tree.right == null)
+                {
+                    if (path.Sum() == this.sum)
+                    {
+                        rtnList.Add(new List<int>(path));
+                    }
+
+                    path.RemoveAt(path.Count-1);
                     return;
                 }
-                if(tree.left != null){
+
+                if (tree.left != null)
+                {
                     traverse(tree.left, path);
                 }
-                if(tree.right != null){
-                    traverse(tree.right,path);
+
+                if (tree.right != null)
+                {
+                    traverse(tree.right, path);
                 }
+
+                path.RemoveAt(path.Count-1);
             }
         }
     }
