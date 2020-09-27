36f3e3031bbd317a283d84bfc9d5cf97d6920dcc
Kami <kidfu@live.com>
2020-09-27 21:44:10 +1300

    traverse the tree

--- Questions/Q113.cs
+++ Questions/Q113.cs
@@ -6,8 +6,10 @@ namespace LeetCodeSolution.Questions
 {
     public class Q113
     {
+        private IList<IList<int>> rtnList;
         public static void Run()
         {
+            rtnList = new List<List<int>>();
             Solution s = new Solution();
             int sum = 22;
             TreeNode root = new TreeNode(5);
@@ -26,7 +28,35 @@ namespace LeetCodeSolution.Questions
 
         public class Solution {
             public IList<IList<int>> PathSum(TreeNode root, int sum) {
-                throw new NotImplementedException();
+                IList<int> path = new List<int>();
+                if(root == null){
+                    return null;
+                }
+                if(root.left == null && root.right == null){
+                    path.add(root.val);
+                    rtnList.add(path);
+                    return rtnList;
+                }
+                if(root.left != null){
+                    traverse(root.left, path);
+                }
+                if(root.right != null){
+                    traverse(root.right, path);
+                }
+                return rtnList;
+            }
+            private void traverse(TreeNode tree, IList<int> path){
+                    path.add(tree.val);
+                if(tree.left == null && tree.right == null){
+                    rtnList.add(path);
+                    return;
+                }
+                if(tree.left != null){
+                    traverse(tree.left, path);
+                }
+                if(tree.right != null){
+                    traverse(tree.right,path);
+                }
             }
         }
     }
