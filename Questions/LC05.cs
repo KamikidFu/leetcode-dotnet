dde3f6281f50f3a1d675a88de8b01ffad5c86843
Kami <kidfu@live.com>
2020-09-30 23:35:05 +1300

    Add LC05

---
+++ Questions/LC05.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LeetCodeSolution.Questions
+{
+    public class LC05
+    {
+        public static void Run()
+        {
+            Solution s =new Solution();
+            string str = "We are happy.";
+            var res = s.ReplaceSpace(str);
+        }
+        public class Solution {
+            public string ReplaceSpace(string s) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char c = s[i];
+                    if (c == ' ')
+                    {
+                        sb.Append("%20");
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
