1b5d65b012fc1694d980a9f7255f332051370c97
Kami <kidfu@live.com>
2020-10-01 21:58:48 +1300

    Add Q154

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            Q240.Run();
+            Q154.Run();
         }
     }
 }
---
+++ Questions/Q154.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeSolution.Questions
+{
+    public class Q154
+    {
+        public static void Run()
+        {
+            Solution s = new Solution();
+            int[] num = new[] {2, 2, 2, 0, 1};
+            var res = s.MinArray(num);
+        }
+        public class Solution {
+            public int MinArray(int[] numbers)
+            {
+                int left = 0, right = numbers.Length-1;
+                while (left < right)
+                {
+                    int mid = (right + left) / 2;
+                    if (numbers[mid] < numbers[right])
+                    {
+                        right = mid;
+                    }else if (numbers[mid] > numbers[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+
+                return numbers[left];
+            }
+        }
+    }
+}
