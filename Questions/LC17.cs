7b7a78a14be9ea49888faf4b530ed583bfa9e2db
Kami <kidfu@live.com>
2020-10-02 21:07:10 +1300

    Add LC17

---
+++ Questions/LC17.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LeetCodeSolution.Questions
+{
+    public class LC17
+    {
+        public static void Run()
+        {
+            Solution s = new Solution();
+            var res = s.PrintNumbers(2);
+        }   
+    }
+    public class Solution {
+        public int[] PrintNumbers(int n)
+        {
+            int num = 1;
+            for (int i = 0; i < n; i++)
+            {
+                num *= 10;
+            }
+
+            int[] res = new int[num - 1];
+            for (int i = 1; i < num; i++)
+            {
+                res[i - 1] = i;
+            }
+
+            return res;
+        }
+    }
+}
