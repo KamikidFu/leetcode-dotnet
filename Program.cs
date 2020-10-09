31f89623d2dfd25a5d77a7f78cc02ffb542034ce
Kami <kidfu@live.com>
2020-10-10 11:53:02 +1300

    Add LC58II

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace LeetCodeSolution
     {
         static void Main(string[] args)
         {
-            LC58.Run();
+            LC58II.Run();
         }
     }
 }
---
+++ Questions/LC58II.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCodeSolution.Questions
+{
+    public class LC58II
+    {
+        public static void Run()
+        {
+            Solution s = new Solution();
+            var res = s.ReverseLeftWords("abcdefg",1);
+            
+        }
+        public class Solution {
+            public string ReverseLeftWords(string s, int n)
+            {
+                string half1 = s.Substring(n);
+                string half2 = s.Substring(0, n);
+                return half1 + half2;
+            }
+        }
+    }
+}
