61ac9819db4f98239421245351650dc23405d487
Kami <kidfu@live.com>
2020-10-11 23:43:33 +1300

    Add LC66

---
+++ Questions/LC66.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolution.Questions
+{
+    public class LC66
+    {
+        public static void Run()
+        {
+            Solution s = new Solution();
+            var res = s.ConstructArr(new []{1,2,3,4,5});
+            
+        }
+        public class Solution {
+            public int[] ConstructArr(int[] a) {
+                int[] res= new int[a.Length];
+                int left = 1;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    res[i] = left;
+                    left *= a[i];
+                }
+
+                int right = 1;
+                for (int i = a.Length - 1; i >= 0; i--)
+                {
+                    res[i] *= right;
+                    right *= a[i];
+                }
+
+                return res;
+            }
+        }
+    }
+}
